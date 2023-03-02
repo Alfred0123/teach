38c4abf62c81d430d7cdef3bff2dbfb347ef4c23
hyobum88 <hyobum.jung@business-canvas.com>
2023-03-02 17:46:57 +0900

    Feat: pg connection

--- server/init/src/controllers/index.ts
+++ server/init/src/controllers/index.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response, NextFunction } from "express";
 import { wrapper } from "../middlewares/wrapper";
 import * as Joi from "joi";
 import { validate } from "../middlewares/validation";
+import { Client } from "pg";
 
 const schema = Joi.object().keys({
   name: Joi.string().required(),
@@ -15,6 +16,8 @@ export class MainController {
     this._router = Router();
 
     this._router.get("/main", validate(schema), wrapper(this.main));
+
+    this._router.get("/pg", wrapper(this.pg));
   }
 
   get router() {
@@ -23,6 +26,25 @@ export class MainController {
 
   public main = async (req: Request, res: Response, next: NextFunction) => {
     console.log({ query: req.query });
+
+    return res.status(200).json({
+      message: "success",
+    });
+  };
+
+  public pg = async (req: Request, res: Response, next: NextFunction) => {
+    console.log("pg");
+
+    const client = await new Client({
+      host: "localhost",
+      port: 5432,
+      user: "postgres",
+      password: "password",
+    });
+    await client.connect();
+
+    const result = await client.query("insert into sample (id) \n values (1)");
+    console.log(result);
 
     return res.status(200).json({
       message: "success",
